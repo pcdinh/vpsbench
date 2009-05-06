607b252dd886bf99ff765f4ec12bd1e2a18e1783
Eivind Uggedal <eu@redflavor.com>
2009-05-06 19:03:26 +0200

    Fixed typo.

--- bencher.py
+++ bencher.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
                                                                   test_dir)
     usr = "testuser"
     pwd = "n0ns3curepWd"
-    tests = "./run-all-tests --slient --small-test " + \
+    tests = "./run-all-tests --silent --small-test " + \
             "--server=Pg --user=%s --password=%s" % (usr, pwd)
     p_cmd = "cd mysql-5.1.34/sql-bench && %s" % tests
 
