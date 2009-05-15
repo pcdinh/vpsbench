e1cedfbf31b220b42c35b2714cc146747ea069f1
Eivind Uggedal <eu@redflavor.com>
2009-05-15 23:16:40 +0200

    System info collection script.

---
+++ sysinfo.py
@@ -0,0 +1,11 @@
+import os
+
+def echo_and_run(cmd):
+    print "# %s" % cmd
+    os.system(cmd)
+    print ""
+
+echo_and_run('uname -nrmo')
+echo_and_run('grep "MemTotal" /proc/meminfo')
+echo_and_run('grep -m 4 -e "model name" -e "MHz" -e "cache size" -e "bogomips" /proc/cpuinfo')
+echo_and_run('grep "processor" /proc/cpuinfo | wc -l')
