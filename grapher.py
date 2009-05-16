9dc858dbb4d2711134432b841bd0dca74479da33
Eivind Uggedal <eu@redflavor.com>
2009-05-16 22:20:36 +0200

    Include amethyst results. Zero pad graph for now.

--- grapher.py
+++ grapher.py
@@ -62,19 +62,23 @@ def graph(results):
     hosts = (
         ('opal.redflavor.com', 'Slicehost'),
         ('garnet.redflavor.com', 'Prgmr'),
-        ('topaz.redflavor.com', 'Linode'),
+        ('topaz.redflavor.com', 'Linode x86_64'),
+        ('amethyst.redflavor.com', 'Linode i686'),
     )
     for test, data in results.items():
         datalist = [data[host[0]] for host in hosts]
 
         plots = []
         dates = []
+        max_points = max([len(d) for d in datalist])
         for hostlist in datalist:
             hostplots = []
             hostdates = []
             for hostitem in hostlist:
                 hostplots.append(hostitem[0])
                 hostdates.append(hostitem[1])
+            if len(hostplots) < max_points:
+                hostplots.extend([0 for i in xrange(max_points-len(hostplots))])
             plots.append(hostplots)
             dates.append(hostdates)
         first_day = dates[0][0]
@@ -96,7 +100,7 @@ def graph(results):
         g = Line(plots, encoding='text')
         g.legend(*[host[1] for host in hosts])
         g.legend_pos('b')
-        g.color("edc240", "afd8f8", "cb4b4b")
+        g.color("edc240", "afd8f8", "cb4b4b", "4da74d")
         for i in range(3):
             g.line(2.5, 1, 0)
         g.size(500, 200)
